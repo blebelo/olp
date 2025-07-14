05a4a1abd5ab726f6270c9495dc41bce85f783cf
Xolani <nathanielmvana@gmail.com>
2025-07-14 10:29:47 +0200

    added Person class that extends User and intructor class that inherits from Person

---
+++ aspnet-core/src/OnlineLearningPlatform.Core/Domain/Person/Instructor/Intructor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineLearningPlatform.Domain.Person;
+
+namespace OnlineLearningPlatform.Domain.Person
+{
+    public class Intructor: Person
+    {
+      public int Id { get; set; }
+    }
+}
