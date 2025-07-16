9c3baf38650bfbbd7c9174746f8f8577e791a4be
=Benny= <blebelo023@student.wethinkcode.co.za>
2025-07-17 00:27:32 +0200

    Removed .ENV Setup For Database Connection

--- aspnet-core/src/OnlineLearningPlatform.EntityFrameworkCore/Migrations/20250716140033_Initialization.cs
+++ aspnet-core/src/OnlineLearningPlatform.EntityFrameworkCore/Migrations/20250716140033_Initialization.cs
@@ -7,7 +7,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace OnlineLearningPlatform.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class Initialization : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
