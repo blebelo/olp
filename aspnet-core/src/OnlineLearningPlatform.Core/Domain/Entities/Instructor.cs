691a390ec2bf033481d4ae3de2fd436eb0e7b8ba
Xolani <nathanielmvana@gmail.com>
2025-07-17 14:12:06 +0200

    refactor: update Instructor entity structure and relationships

--- aspnet-core/src/OnlineLearningPlatform.Core/Domain/Entities/Instructor.cs
+++ aspnet-core/src/OnlineLearningPlatform.Core/Domain/Entities/Instructor.cs
@@ -1,10 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using OnlineLearningPlatform.Authorization.Roles;
+using OnlineLearningPlatform.Authorization.Users;
+using System.Collections.Generic;
 
 namespace OnlineLearningPlatform.Domain.Entities
 {
     public class Instructor : Person
     {
+
+        //public virtual User User { get; set; }
         public string Profession { get; set; }
-        public ICollection<Course> CoursesCreated { get; set; }
+        public virtual ICollection<Course> CoursesCreated { get; set; }
+
+
+        //Initializing the CoursesCreated collection to avoid null reference exceptions
+        public Instructor()
+        {
+
+            CoursesCreated = new List<Course>();
+        }
     }
 }
