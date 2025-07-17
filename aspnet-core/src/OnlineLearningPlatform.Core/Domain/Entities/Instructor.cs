e952e1c06aa835583932d629da6048fd059927a7
Xolani <nathanielmvana@gmail.com>
2025-07-17 13:41:41 +0200

    refactor: Instructor to reflect model

--- aspnet-core/src/OnlineLearningPlatform.Core/Domain/Entities/Instructor.cs
+++ aspnet-core/src/OnlineLearningPlatform.Core/Domain/Entities/Instructor.cs
@@ -1,10 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using Abp.Domain.Entities.Auditing;
+using OnlineLearningPlatform.Authorization.Users;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OnlineLearningPlatform.Domain.Entities
 {
-    public class Instructor : Person
+
+    public abstract class Person : FullAuditedEntity<Guid>
     {
-        public string Profession { get; set; }
-        public ICollection<Course> CoursesCreated { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        [NotMapped]
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        [NotMapped]
+        public string Password { get; set; }
+        public string Bio { get; set; }
+        public User UserAccount { get; set; }
     }
 }
