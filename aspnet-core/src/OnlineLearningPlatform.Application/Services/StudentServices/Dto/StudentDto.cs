6d66fb256ee8bca9f74d5d9a7b9fe739c657db36
Bokang <bokangngoetjana06@gmail.com>
2025-07-18 15:21:21 +0200

    chore: Added automapping to DTOs

--- aspnet-core/src/OnlineLearningPlatform.Application/Services/StudentServices/Dto/StudentDto.cs
+++ aspnet-core/src/OnlineLearningPlatform.Application/Services/StudentServices/Dto/StudentDto.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using OnlineLearningPlatform.Domain.Students;
 
 namespace OnlineLearningPlatform.Services.StudentServices.Dto
 {
+    [AutoMap(typeof(Student))]
     public class StudentDto : FullAuditedEntityDto<Guid>
     {
         public string Name { get; set; }
         public string Surname { get; set; }
-        public string Email { get; set; }
-        public string Username { get; set; }
         public string Interests { get; set; }
         public string AcademicLevel { get; set; }
         public int EnrolledCoursesCount { get; set; }
