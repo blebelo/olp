50b99341e87148e4b3dba5a434afcd55b703950a
Bokang <bokangngoetjana06@gmail.com>
2025-07-20 00:34:18 +0200

    chore: added the landing page ui

---
+++ front-end/src/components/landingPage/LandingPage.tsx
@@ -0,0 +1,59 @@
+'use client';
+import React from 'react';
+import { Typography, Button, Row, Col, Space } from 'antd';
+import { SearchOutlined, PlayCircleOutlined } from '@ant-design/icons';
+import styles from './LandingPage.module.css';
+
+const { Title, Paragraph } = Typography;
+
+const LandingPage: React.FC = () => {
+  return (
+    <div className={styles.heroContainer}>
+      <div className={styles.decorativeCircleLarge}></div>
+      <div className={styles.decorativeCircleSmall}></div>
+
+      <Row justify="center" align="middle" className={styles.contentRow}>
+        <Col xs={24} sm={24} md={12} lg={11} xl={10}>
+          <div className={styles.textContainer}>
+            <Title level={1} className={styles.mainTitle}>
+              Learn with Us Anytime, Anywhere!
+            </Title>
+            
+            <Paragraph className={styles.subtitleText}>
+              Click the buttons below to sign up as an instructor or a student
+            </Paragraph>
+            
+            <Space size="large" wrap>
+              <Button 
+                type="primary" 
+                size="large"
+                icon={<SearchOutlined />}
+                className={styles.primaryButton}
+              >
+                Instructor
+              </Button>
+              
+              <Button 
+                size="large"
+                icon={<PlayCircleOutlined />}
+                className={styles.secondaryButton}
+              >
+                Student
+              </Button>
+            </Space>
+          </div>
+        </Col>
+        
+        <Col xs={24} sm={24} md={12} lg={13} xl={14}>
+          <div className={styles.imageContainer}>
+            <div className={styles.personPlaceholder}>
+              👨‍💻
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default LandingPage;
