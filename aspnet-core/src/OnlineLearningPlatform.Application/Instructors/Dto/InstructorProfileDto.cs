abb9668e7e8e91cabe25ec0fd846bf638ff606df
Xolani <nathanielmvana@gmail.com>
2025-07-19 15:07:04 +0200

    feature: added func for instructor profile view and update

---
+++ aspnet-core/src/OnlineLearningPlatform.Application/Instructors/Dto/InstructorProfileDto.cs
@@ -0,0 +1,22 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using OnlineLearningPlatform.Domain.Instructors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLearningPlatform.Instructors.Dto
+{
+    [AutoMapFrom(typeof(Instructor))]
+    public class InstructorProfileDto: EntityDto<Guid>
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Bio { get; set; }
+        public string Profession { get; set; }
+    }
+}
