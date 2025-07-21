b983648c3c3ef1cdcbeb904c423b540fdf62a184
Benny <tarr184pro@gmail.com>
2025-07-21 14:30:06 +0200

    Implemented Get Courses By Instrutor Functiinality

--- aspnet-core/src/OnlineLearningPlatform.Application/Instructors/IInstructorAppService.cs
+++ aspnet-core/src/OnlineLearningPlatform.Application/Instructors/IInstructorAppService.cs
@@ -1,10 +1,14 @@
 ﻿using Abp.Application.Services;
+using OnlineLearningPlatform.Courses.Dto;
 using OnlineLearningPlatform.Instructors.Dto;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace OnlineLearningPlatform.Instructors
 {
     public interface IInstructorAppService: IAsyncCrudAppService<CreateInstructorDto, Guid>
     {
+        public Task<ICollection<CourseDto>> GetCoursesAsync(Guid instructorId);
     }
 }
--- aspnet-core/src/OnlineLearningPlatform.Application/Instructors/InstructorAppService.cs
+++ aspnet-core/src/OnlineLearningPlatform.Application/Instructors/InstructorAppService.cs
@@ -2,14 +2,16 @@
 using Abp.Domain.Repositories;
 using Abp.UI;
 using OnlineLearningPlatform.Authorization.Users;
+using OnlineLearningPlatform.Courses.Dto;
 using OnlineLearningPlatform.Domain.Instructors;
 using OnlineLearningPlatform.Instructors.Dto;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace OnlineLearningPlatform.Instructors
 {
-    public class InstructorAppService : AsyncCrudAppService<Instructor, CreateInstructorDto, Guid>
+    public class InstructorAppService : AsyncCrudAppService<Instructor, CreateInstructorDto, Guid>, IInstructorAppService
     {
         private readonly IRepository<Instructor, Guid> _instructorRepository;
         private readonly UserManager _userManager;
@@ -35,5 +37,18 @@ namespace OnlineLearningPlatform.Instructors
             return input;
         }
 
+        public async Task<ICollection<CourseDto>> GetCoursesAsync(Guid instructorId)
+        {
+            var instructor = await _instructorRepository.GetAsync(instructorId);
+            var courses = instructor.CoursesCreated;
+
+            if (courses == null || courses.Count == 0)
+            {
+                throw new UserFriendlyException("No courses found for this instructor.");
+            }
+            var listOfCourses = ObjectMapper.Map<List<CourseDto>>(courses);
+
+            return listOfCourses;
+        }
     }
 }
