3bcdfb075b0fcb408454a8fade5a51f431270b4e
karabomodise260@gmail.com <karabomodise260@gmail.com>
2025-07-25 04:49:23 +0200

    added the functionality to lo0ad enrolled courses

--- front-end/src/app/student/progress/page.tsx
+++ front-end/src/app/student/progress/page.tsx
@@ -1,11 +1,38 @@
 'use client';
+import { useEffect } from 'react';
 import { useStyles } from './Style/style';
-import { Button, Progress, Typography } from 'antd';
-import { courses } from '@/utils/sample-courses/courses';
-
+import { Button, Typography } from 'antd';
+import { useStudentEnrollmentActions, useStudentEnrollmentState } from "@/providers/enrollment-provider";
+import { useRouter } from "next/navigation";
+import { ICourseDtos } from '@/providers/enrollment-provider/context';
 
 export default function StudentProgressPage() {
     const { styles } = useStyles();
+    const { getStudentEnrolledCourses } = useStudentEnrollmentActions();
+    const { enrolledCourses } = useStudentEnrollmentState();
+    const router = useRouter();
+
+
+    useEffect(() => {
+        const storedId = sessionStorage.getItem("Id");
+        if (storedId !== null) {
+            const numericId = parseInt(storedId, 10);
+            if (!isNaN(numericId)) {
+                getStudentEnrolledCourses(numericId);
+            }
+        }
+    }, []);
+
+    const handleCourseClick = (course: ICourseDtos) => {
+
+        const isEnrolled = enrolledCourses?.some((enrolled) => enrolled.id === course.id);
+        if (isEnrolled) {
+            router.push(`/student/enrolled-course/${course.id}`);
+
+            return;
+        }
+    }
+
     const { Title, Paragraph } = Typography;
     return (
         <div className={styles.pageContainer}>
@@ -14,29 +41,16 @@ export default function StudentProgressPage() {
             <Title className={styles.pageTitle}>My Course Progress</Title>
 
             <div className={styles.courseList}>
-                {courses.map((course) => (
+                {enrolledCourses?.map((course) => (
                     <div key={course.id} className={styles.courseCard}>
                         <div className={styles.courseDetails}>
                             <Title level={4} className={styles.courseTitle}>{course.title}</Title>
                             <Paragraph className={styles.courseDescription}>{course.description}</Paragraph>
-
-                            <div className={styles.progressContainer}>
-                                <Progress
-                                    percent={course.completion}
-                                    status={course.completion === 100 ? 'success' : 'active'}
-                                    className={styles.progressBar}
-                                />
-                                <span className={styles.completionText}>
-                                    {course.completion === 100 && <span className={styles.completed}>Completed</span>}
-                                </span>
-                            </div>
                         </div>
-
-                        <div className={styles.actionSection}>
-                            <Button type="primary" className={styles.actionButton}>
-                                {course.completion === 100 ? 'View Course' : 'Continue Learning'}
-                            </Button>
-                        </div>
+                            <Button type="primary" className={styles.actionButton} onClick={() => handleCourseClick(course)}>
+                                {/* {course.completion === 100 ? 'View Course' : 'Continue Learning'} */}
+                                Continue
+                            </Button>                   
                     </div>
                 ))}
             </div>
